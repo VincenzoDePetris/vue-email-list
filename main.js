37c5123021b9d43dbf7111291ddb1d62f28a160c
VincenzoDePetris <vince02@live.it>
2023-09-07 10:00:59 +0200

    fix

--- main.js
+++ main.js
@@ -3,21 +3,18 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      randomMail: [],
+      mails: [],
     };
   },
   methods: {
     generateMail() {
-      this.randomMail = [];
       for (let i = 0; i < 10; i++) {
         axios
           .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((response) => {
-            const Mail = response.data.response;
-            this.randomMail = Mail;
+            const generateMail = response.data.response;
+            this.mails.push(generateMail);
           });
-        this.randomMail.push(this.Mail);
-        console.log(this.randomMail);
       }
     },
   },
